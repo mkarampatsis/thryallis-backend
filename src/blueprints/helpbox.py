f7ba0152fc09caec982a9ef0e978151e44674309
Markos Karampatsis <marka@central.ntua.gr>
2025-05-19 11:06:49 +0300

    Change to helpbox

--- src/blueprints/helpbox.py
+++ src/blueprints/helpbox.py
@@ -158,95 +158,95 @@ def retrieve_all_published_questions():
 @jwt_required()
 def create_question():
 
-    emails = User.objects(roles__role='HELPDESK').only('email').exclude("id")
-    # print (emails.to_json())
+  emails = User.objects(roles__role='HELPDESK').only('email').exclude("id")
+  # print (emails.to_json())
 
-    pipeline = [
-        {
-            "$group":{
-                "_id": {"email":"$toWhom.email"},
-                "countNumberOfDocuments": {"$count": {}}
-            }
-        },
-        {
-            "$project": {
-                "_id": 0,
-                "email":"$_id.email",
-                "countNumberOfDocuments":1,
-                "count": {"$size":"$questions"}
-            }
-        }
-    ]
+  pipeline = [
+    {
+      "$group":{
+        "_id": {"email":"$toWhom.email"},
+        "countNumberOfDocuments": {"$count": {}}
+      }
+    },
+    {
+      "$project": {
+        "_id": 0,
+        "email":"$_id.email",
+        "countNumberOfDocuments":1,
+        "count": {"$size":"$questions"}
+      }
+    }
+  ]
 
-    document_counts = list(Helpbox.objects.aggregate(pipeline))
-    # print (document_counts)
+  document_counts = list(Helpbox.objects.aggregate(pipeline))
+  # print (document_counts)
 
-    # Create a lookup for document counts
-    count_dict = {entry['email']: entry['countNumberOfDocuments'] for entry in document_counts}
+  # Create a lookup for document counts
+  count_dict = {entry['email']: entry['countNumberOfDocuments'] for entry in document_counts}
 
-    # Assign a default high value (e.g., float('inf')) for emails not in document_counts
-    all_counts = [{'email': email['email'], 'countNumberOfDocuments': count_dict.get(email['email'], 0)} for email in emails]
+  # Assign a default high value (e.g., float('inf')) for emails not in document_counts
+  all_counts = [{'email': email['email'], 'countNumberOfDocuments': count_dict.get(email['email'], 0)} for email in emails]
 
-    # Find the email with the lowest countNumberOfDocuments
-    email_with_lowest_count = min(all_counts, key=lambda x: x['countNumberOfDocuments'])
-    helpdeskUser = User.objects.get(email=email_with_lowest_count['email'])
+  # Find the email with the lowest countNumberOfDocuments
+  email_with_lowest_count = min(all_counts, key=lambda x: x['countNumberOfDocuments'])
+  helpdeskUser = User.objects.get(email=email_with_lowest_count['email'])
 
-    # print("Email with lowest countNumberOfDocuments:", email_with_lowest_count['email'])
+  # print("Email with lowest countNumberOfDocuments:", email_with_lowest_count['email'])
 
-    try:
-        data = request.get_json()
-        debug_print("POST HELPBOX", data)
+  try:
+    data = request.get_json()
+    debug_print("POST HELPBOX", data)
+    fileObjectIDs = []
 
-        files = data["question"]['questionFile']
-        
-        if files:
-            # Convert to ObjectId instances
-            fileObjectIDs = [ObjectId(id_str) for id_str in files]
-        
-        email = data["email"]
-        lastName = data["lastName"]
-        firstName = data["firstName"]
-        organizations = data["organizations"]
-        questionTitle = data["questionTitle"]
-        questionCategory = data["questionCategory"]
-        question = [{
-            "questionText": data["question"]['questionText'],
-            "questionFile": fileObjectIDs
-        }]
-        toWhom = {
-          "email" : helpdeskUser["email"],
-          "firstName" : helpdeskUser["firstName"],
-          "lastName" : helpdeskUser["lastName"]
-        }
+    files = data["question"]['questionFile']
+    if files:
+      # Convert to ObjectId instances
+      fileObjectIDs = [ObjectId(id_str) for id_str in files]
+    
+    email = data["email"]
+    lastName = data["lastName"]
+    firstName = data["firstName"]
+    organizations = data["organizations"]
+    questionTitle = data["questionTitle"]
+    questionCategory = data["questionCategory"]
+    question = [{
+      "questionText": data["question"]['questionText'],
+      "questionFile": fileObjectIDs
+    }]
+    toWhom = {
+      "email" : helpdeskUser["email"],
+      "firstName" : helpdeskUser["firstName"],
+      "lastName" : helpdeskUser["lastName"]
+    }
 
-        length = 6
-        random_string = ''.join(random.choices(string.digits, k=length))
+    length = 6
+    random_string = ''.join(random.choices(string.digits, k=length))
 
-        newHelpbox = Helpbox(
-            key = random_string,
-            email=email,
-            lastName=lastName,
-            firstName=firstName,
-            organizations=organizations,
-            questionTitle = questionTitle,
-            questionCategory = questionCategory,
-            questions = question,
-            toWhom = toWhom         
-        ).save()
+    newHelpbox = Helpbox(
+      key = random_string,
+      email=email,
+      lastName=lastName,
+      firstName=firstName,
+      organizations=organizations,
+      questionTitle = questionTitle,
+      questionCategory = questionCategory,
+      questions = question,
+      toWhom = toWhom         
+    ).save()
 
-        return Response(
-            json.dumps({"message": "Η ερώτηση σας καταχωρήθηκε με επιτυχία"}),
-            mimetype="application/json",
-            status=201,
-        )
+    return Response(
+      json.dumps({"message": "Η ερώτηση σας καταχωρήθηκε με επιτυχία"}),
+      mimetype="application/json",
+      status=201,
+    )
 
-    except Exception as e:
-        print(e)
-        return Response(
-            json.dumps({"message": f"<strong>Αποτυχία καταχώρησης ερώτησης:</strong> {e}"}),
-            mimetype="application/json",
-            status=500,
-        )
+  except Exception as e:
+    print(e)
+    return Response(
+      json.dumps({"message": f"<strong>Αποτυχία καταχώρησης ερώτησης:</strong> {e}"}),
+      mimetype="application/json",
+      status=500,
+    )
 
 @helpbox.route("/<string:id>", methods=["PUT"])
 @jwt_required()
@@ -342,41 +342,41 @@ def answer_question():
 @helpbox.route("/publish", methods=["PUT"])
 @jwt_required()
 def publish_question():
-    try:
-        data = request.get_json()
-        debug_print("PUBLISH HELPBOX", data)
+  try:
+    data = request.get_json()
+    debug_print("PUBLISH HELPBOX", data)
 
-        helpboxId = data["helpBoxId"]
-        questionId = data["questionId"]
-        published = data["published"]
+    helpboxId = data["helpBoxId"]
+    questionId = data["questionId"]
+    published = data["published"]
 
-        helpbox = Helpbox.objects.get(id=ObjectId(helpboxId))
-        print (helpbox.to_json())
-        
-        if helpbox:
-            # Locate the specific Question in the questions list
-            question_to_update = next((q for q in helpbox.questions if str(q.id) == questionId), None)
-            
-            if question_to_update:
-                # Update the `answered` field
-                question_to_update.published = published
-                                
-                # Save the document to persist changes
-                helpbox.save()
+    helpbox = Helpbox.objects.get(id=ObjectId(helpboxId))
+    print (helpbox.to_json())
+    
+    if helpbox:
+      # Locate the specific Question in the questions list
+      question_to_update = next((q for q in helpbox.questions if str(q.id) == questionId), None)
+      
+      if question_to_update:
+        # Update the `answered` field
+        question_to_update.published = published
+                        
+        # Save the document to persist changes
+        helpbox.save()
 
-        return Response(
-            json.dumps({"message": "Η απάντηση σας καταχωρήθηκε με επιτυχία"}),
-            mimetype="application/json",
-            status=201,
-        )
+    return Response(
+      json.dumps({"message": "Η απάντηση σας καταχωρήθηκε με επιτυχία"}),
+      mimetype="application/json",
+      status=201,
+    )
 
-    except Exception as e:
-        print(e)
-        return Response(
-            json.dumps({"message": f"<strong>Αποτυχία καταχώρησης απάντησης:</strong> {e}"}),
-            mimetype="application/json",
-            status=500,
-        )
+  except Exception as e:
+    print(e)
+    return Response(
+      json.dumps({"message": f"<strong>Αποτυχία καταχώρησης απάντησης:</strong> {e}"}),
+      mimetype="application/json",
+      status=500,
+    )
 
 @helpbox.route("/finalize", methods=["PUT"])
 @jwt_required()
