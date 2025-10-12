03d70ee85fa4527f0053d8e7e53905d606cbaa6b
mkarampatsis <marka@central.ntua.gr>
2025-10-12 18:38:05 +0300

    changes to conf

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -avz -e 'ssh -i /home/markos/.ssh/id_rsa_ch' --delete apografi_sync*.py psped_sync*.py requirements.txt src wsgi.py ypes-backend:flask
-ssh -o "IdentitiesOnly=yes" -i /home/markos/.ssh/id_rsa_ch ypes-backend 'source ~/flask/venv/bin/activate && pip install -r ~/flask/requirements.txt'
+rsync -avz --delete apografi_sync*.py psped_sync*.py apografi_check*.py count_distinct.py requirements.txt src wsgi.py lola:ypes-backend
+ssh lola 'source ~/ypes-backend/venv/bin/activate && pip install -r ~/ypes-backend/requirements.txt'
 ssh oper@lola 'sudo supervisorctl reload'
