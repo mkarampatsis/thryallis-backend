775baf9a6c4a2c4dd906f329042ae5d0fcefb977
mkarampatsis <marka@central.ntua.gr>
2025-10-13 21:26:51 +0300

    Change to cofog

--- src/blueprints/cofog.py
+++ src/blueprints/cofog.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, Response
 from src.models.cofog import Cofog
 import json
+from bson import json_util
 
 cofog = Blueprint("cofog", __name__)
 
@@ -8,11 +9,25 @@ cofog = Blueprint("cofog", __name__)
 @cofog.route("")
 def get_cofog():
     try:
-        cofog = Cofog.objects()
-        return Response(cofog.to_json(), mimetype="application/json", status=200)
+      cofog = Cofog.objects.aggregate([
+        {
+          "$addFields": {
+              "codeInt": { "$toInt": "$code" }
+          }
+        },
+        {
+          "$sort": { "codeInt": 1 }
+        }
+      ]) 
+      cofog_list = list(cofog)
+      return Response(
+        json_util.dumps({"data": cofog_list}),
+        mimetype="application/json",
+        status=200,
+    )
     except Cofog.DoesNotExist:
-        return Response(
-            json.dumps({"error": "Δεν βρέθηκαν δεδομένα COFOG"}),
-            mimetype="application/json",
-            status=404,
-        )
+      return Response(
+          json.dumps({"error": "Δεν βρέθηκαν δεδομένα COFOG"}),
+          mimetype="application/json",
+          status=404,
+      )
